51cb96caaf441d66e0654ecf921b9e2497aa85a1
Amel Hassan <t-amhass@microsoft.com>
2019-07-31 17:07:48 -0700

    Updated JavaScript Tests

--- JavaScript_Tests/HttpTrigger/index.js
+++ JavaScript_Tests/HttpTrigger/index.js
@@ -1,16 +1,46 @@
 module.exports = async function (context, req, TokenBindingOutput) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  // Note: console is local debug, context is azure portal debug
+  var fs = require('file-system');
+  var fetch = require('isomorphic-fetch');
+  var Dropbox = require('dropbox').Dropbox;
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+  context.res = {
+    status: 400,
+    body: "Check your dropbox account to see a new document listing all your files."
+    };
+    
+  dbx = new Dropbox({
+    fetch: fetch,
+    accessToken: TokenBindingOutput  
+    })
+
+  .filesListFolder({path: ''}) // returns a promise for Dropboxtypes.files
+  .then(response => { 
+    var datastring = "Files: ";
+    for (let i = 0; i < response.entries.length; i++){
+      console.log(response.entries[i].name);
+      context.log(response.entries[i].name); 
+      datastring += response.entries[i].name + ", ";
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body" + (TokenBindingOutput)
-        };
-    }
-};
+      dbx = new Dropbox({
+        fetch: fetch,
+        accessToken: TokenBindingOutput
+        })
+    
+      .filesUpload({path: "/listdropboxfiles", contents: datastring}).then(function(response){
+        console.log("Check your dropbox account to see a new document listing all your files.");
+        context.log("Check your dropbox account to see a new document listing all your files.");
+        console.log(response);        
+      })
+      .catch(function(error){
+        console.error(error);
+        context.log(error);
+      });
+  })
+  .catch(function(error){
+    console.error(error);
+    context.log("An error occurred in outer catch statement");
+    context.log(error);
+  });
+    
+}
